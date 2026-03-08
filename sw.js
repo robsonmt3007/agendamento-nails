a559fa68d98a74aa068b9683c04125a0387df8a7
robsonmt3007 <steevsnow8@gmail.com>
2026-03-08 09:13:22 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nails-rita-v40';
+const CACHE_NAME = 'nails-rita-v41';
 const assets = [
   './',
   './index.html',
@@ -10,29 +10,19 @@ const assets = [
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
-  self.skipWaiting(); // Força a atualização imediata
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(assets)));
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
-      return Promise.all(
-        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
-      );
+      return Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)));
     })
   );
-  self.clients.claim(); // Garante que o SW controle a página imediatamente
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.match(event.request).then((res) => res || fetch(event.request)));
 });

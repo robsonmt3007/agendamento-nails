c6ed35b3d8bdf9396db21e7dac987baf9c22ce36
robsonmt3007 <steevsnow8@gmail.com>
2026-03-07 21:06:24 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // --- CONFIGURAÇÃO INICIAL ---
 // Sempre que você mudar o código do index.html ou quiser forçar uma atualização,
 // mude o número abaixo (ex: nails-v25 para nails-v26)
-const CACHE_NAME = 'nails-v33';
+const CACHE_NAME = 'nails-v34';
 
 // Lista de todos os arquivos que o app precisa para funcionar offline
 const ASSETS = [
